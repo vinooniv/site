00d1f249d88ec95c9e2daa52d1011c72aab9c6e0
vinooniv <vinooniv90@gmail.com>
2020-05-15 23:27:18 +0530

    adding custom 404 page

---
+++ src/pages/404.vue
@@ -0,0 +1,17 @@
+<template>
+  <Layout>
+    <v-layout>
+      <v-flex>
+        <h1>Not found</h1>
+      </v-flex>
+    </v-layout>
+  </Layout>
+</template>
+
+<script>
+export default {
+  metaInfo: {
+    title: '404'
+  }
+}
+</script>
