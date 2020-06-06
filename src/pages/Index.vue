591b1854e28c402d280ba571c1b01525468cdc74
vinooniv <vinooniv90@gmail.com>
2020-06-06 19:20:59 +0530

    added home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,23 +1,45 @@
 <template>
   <Layout>
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
+    <v-container grid-list-xl>
+      <v-layout row align-center>
+        <v-flex lg6 xs12>
+          <h1>Hello, I am Vino!</h1>
+          <br>
+          <p>
+            A Computer Vision, Deep Learning and Quality Assurance engineer. Passionate to solve problems through code
+          </p>
+        </v-flex>
+        <v-flex lg6 xs12>
+          <img :src="require('~/assets/img/creativity.svg')">
+        </v-flex>
+      </v-layout>
+      <v-layout grid-list-xl>
+        <v-flex lg12>
+          <div class="contact">
+            Have questions? <v-btn outlined small href="mailto:contact@vinomahendran.com">CONTACT ME</v-btn>
+          </div>
+        </v-flex>
+      </v-layout>
+    </v-container>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: 'Hello'
+    title: 'Home'
   }
 }
 </script>
 
-<style>
-.home-links a {
-  margin-right: 1rem;
+<style scoped>
+p {
+  font-size: 1.5rem;
+  line-height: 1.3;
+  text-justify: distribute;
+}
+.contact {
+  font-size: 1.2rem;
+  text-align: center;
 }
 </style>
